800c22cb17cc53e9d64502224f766458ed88a851
manderson3606 <manderson3606@gmail.com>
2026-07-13 15:53:11 -0700

    chore: update msalConfig authority to common endpoint

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@
 export const msalConfig = {
   auth: {
     clientId: 'f3699af3-13a6-42c4-804d-7bbc7a2f432c',
-    authority: 'https://login.microsoftonline.com/fef83eaf-f0ca-417c-a714-2881daf9624e',
+    authority: 'https://login.microsoftonline.com/common',
     redirectUri: window.location.origin,
   },
   cache: {
